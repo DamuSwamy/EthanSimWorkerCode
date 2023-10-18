6960235d9d24604e392432d75474abadaf2e6e05
Damodaran NJ <damodaran.swamy@accelnomics.com>
2023-10-18 23:00:45 +0000

    Changes on 20231018

--- sim-dev-ssc01/packs/sim_sec_alerts/actions/get_network_customer_device_assets.py
+++ sim-dev-ssc01/packs/sim_sec_alerts/actions/get_network_customer_device_assets.py
@@ -4,7 +4,7 @@ import requests
 class GetNetDbDetails(Action):
     def run(self):
         try:
-            url = "https://netdb.essist.com.au/api/v1/cpe/assurancedashboard/?customer=36"
+            url = "https://netdb.essist.com.au/api/v1/asset/assurancedashboard/?customer=36"
             auth_token = "0ab414e6487e35477157f38640f58e8ae6a0df89"
 
             method = 'get'
@@ -16,28 +16,30 @@ class GetNetDbDetails(Action):
 
             response = requests.request(method, url, headers=headers)
             response.raise_for_status()  # Raise an exception for HTTP error responses
-
+            
             data = response.json()
 
             formatted_data = []
             primary_id = 1
 
-            for entry in data:
+            for entry in data:  # Loop through entries in the 'body' list
                 formatted_entry = {
                     "PrimaryID": str(primary_id),
                     "CustomerID": str(entry.get("customer_id", "")),
-                    "SiteID": str(entry.get("site_id", "")),
-                    "DeviceID": str(entry.get("id", "")),
-                    "Managed": str(entry.get("managed", "")),
-                    "Monitored": str(entry.get("monitored", "")),
-                    "CPEType": str(entry.get("cpetype", "")),
-                    "Manufacturer": str(entry.get("manufacturer", "")),
-                    "DeviceName": str(entry.get("devicename", "")),
-                    "IPAddress": str(entry.get("mgmt_ipv4", "")),
-                    "Monitor_SW_NodeID": str(entry.get("monitor_sw_nodeid", "")),
-                    "SNSRCount": str(entry.get("servicenowcase_counts", {}).get("service_request", "")),
-                    "SNEventCount": str(entry.get("servicenowcase_counts", {}).get("event", "")),
-                    "SNIncidentCount": str(entry.get("servicenowcase_counts", {}).get("incident", ""))
+                    "DeviceID": str(entry.get("cpe_id", "")),
+                    "AssetID": str(entry.get("id", "")),
+                    "ProductID": str(entry.get("productcode", "")),
+                    "ProductName": str(entry.get("productname", "")),
+                    "Serial": str(entry.get("serial_number", "")),
+                    "WarrantyDate": str(entry.get("warranty_end_date", "")),
+                    "Model": str(entry.get("productname", "")),
+                    "LastDateSupport": str(entry.get("eol_last_date_support", "")),
+                    "Message": "",
+                    "ModifiedOn": str(entry.get("lastupdated", "")),
+                    "Status": str(entry.get("active", "")),
+                    "SmartNetCoverageEndDate": str(entry.get("support_coverage_end_date", "")),
+                    "SmartNetContractType": str(entry.get("support_contract_type", "")),
+                    "SNAssetSysID": str(entry["servicenow"].get("ethanprod_asset", ""))
                 }
                 formatted_data.append(formatted_entry)
                 primary_id += 1
