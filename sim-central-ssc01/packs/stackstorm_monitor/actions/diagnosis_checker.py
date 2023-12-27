8e28582f2ba9b46c4fcea19aec621916378da9e7
Damodaran NJ <damodaran.swamy@accelnomics.com>
2023-12-27 23:00:46 +0000

    Changes on 20231227

--- sim-central-ssc01/packs/stackstorm_monitor/actions/diagnosis_checker.py
+++ sim-central-ssc01/packs/stackstorm_monitor/actions/diagnosis_checker.py
@@ -5,24 +5,13 @@ from st2common.runners.base_action import Action
 
 class MyCustomStackStormAction(Action):
     def run(self):
-
-        # Get yesterday's date
-        yesterday_date = datetime.now() - timedelta(days=1)
-        yesterday_date_str = yesterday_date.strftime("%Y-%m-%d")
-        today_date = datetime.now()
-        today_date_str = today_date.strftime("%Y-%m-%d")
-
-        ist_offset = timedelta(hours=5, minutes=30)
-        start_time = f"{yesterday_date_str}T09:30:00"
-        end_time = f"{today_date_str}T09:29:59"
+        today_date = datetime.now(timezone.utc)
+        today_date_ist = today_date + timedelta(hours=5, minutes=30)
+        yesterday_9_30_am_ist = today_date_ist.replace(hour=9, minute=30, second=0, microsecond=0) - timedelta(days=1)
+        today_9_30_am_ist = today_date_ist.replace(hour=9, minute=30, second=0, microsecond=0)
 
         diagnosis_count = 0
 
-        # Convert local time to UTC
-        start_time_utc = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S").replace(tzinfo=timezone.utc) - ist_offset
-        end_time_utc = datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%S").replace(tzinfo=timezone.utc) - ist_offset
-
-        # Specify action names to fetch
         action_names_to_fetch = ["sim_core.catch_all_automation_create_user", "sim_core.catch_all_automation_v2"]
 
         base_url = "https://sim-central.sim.esecure.systems/api/v1/executions?"
@@ -34,62 +23,46 @@ class MyCustomStackStormAction(Action):
 
         try:
             for action_name in action_names_to_fetch:
-                # Construct the updated URL with the current action name and status parameter
-                url = base_url + f"action={action_name}&status=succeeded"
+                url = base_url + f"action={action_name}"
 
                 response = requests.get(url, headers=headers)
                 if response.status_code == 200:
                     data = json.loads(response.text)
 
                     for execution in data:
-                        start_timestamp_str = execution.get("start_timestamp")
+                        start_timestamp_str = execution.get("start_timestamp", "")
+                        start_timestamp = datetime.strptime(start_timestamp_str, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
+                        start_timestamp_ist = start_timestamp + timedelta(hours=5, minutes=30)
 
-                        # Check if start_timestamp_str is not None
-                        if start_timestamp_str is not None:
-                            # Convert timestamp string to datetime object
-                            start_timestamp_utc = datetime.strptime(start_timestamp_str, "%Y-%m-%dT%H:%M:%S.%fZ").replace(
-                                tzinfo=timezone.utc
-                            )
+                        if yesterday_9_30_am_ist <= start_timestamp_ist <= today_9_30_am_ist:
+                            id = execution["id"]
+                            status = execution.get("status")
+                            start_time = start_timestamp_ist.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
-                            # Adjust the start timestamp to IST
-                            start_timestamp_ist = start_timestamp_utc + ist_offset
+                            end_timestamp_str = execution.get("end_timestamp", "")
+                            end_timestamp = datetime.strptime(end_timestamp_str, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
+                            end_timestamp_ist = end_timestamp + timedelta(hours=5, minutes=30)
+                            end_time = end_timestamp_ist.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
-                            # Check if the execution time is within the desired range
-                            if start_time_utc <= start_timestamp_ist <= end_time_utc:
-                                id = execution["id"]
-                                status = execution.get("status", "N/A")
-                                end_timestamp_str = execution.get("end_timestamp", "N/A")
-                                end_timestamp_utc = datetime.strptime(end_timestamp_str, "%Y-%m-%dT%H:%M:%S.%fZ").replace(
-                                    tzinfo=timezone.utc
-                                )
-                                end_timestamp_ist = end_timestamp_utc + ist_offset
-                                end_time = end_timestamp_ist.strftime("%Y-%m-%dT%H:%M:%S")
+                            state = execution.get("result", {}).get("output", {}).get("output", {}).get("info", {}).get("state", "N/A")
+                            ticket_number = execution.get("result", {}).get("output", {}).get("output", {}).get("info", {}).get("info", {}).get("ticket", "N/A")
 
-            
-                                state = execution.get("result", {}).get("output", {}).get("output", {}).get("info", {}).get(
-                                    "state", "N/A"
-                                )
-                                ticket_number = execution.get("result", {}).get("output", {}).get("output", {}).get("info", {}).get(
-                                    "info", {}
-                                ).get("ticket", "N/A")
-
-                                if state.lower() == "diagnosis":
-                                    diagnosis_count += 1
-                                    print("Execution_id:", id)
-                                    print("Action Name:", action_name)
-                                    print("Execution start time(IST):", start_timestamp_ist.strftime("%Y-%m-%dT%H:%M:%S"))
-                                    print("Execution End time(IST):", end_time)
-                                    print("Ticket Number", ticket_number)
-                                    print("-----------------------------------------")
+                            if state.lower() == "diagnosis":
+                                diagnosis_count += 1
+                                print("Execution_id:", id)
+                                print("Action Name:", action_name)
+                                print("Execution start time(IST):", start_time)
+                                print("Execution End time(IST):", end_time)
+                                print("Ticket Number", ticket_number)
                 else:
                     print(f"Request for action {action_name} failed with status code {response.status_code}")
                     print("Response content:", response.text)
 
+            if diagnosis_count != 0:
+                print("Diagnosis Count:", diagnosis_count)
+            else:
+                print(f"No diagnosis found between {yesterday_9_30_am_ist} and {today_9_30_am_ist}")
+
         except Exception as e:
             print(f"An error occurred: {e}")
-
-        if diagnosis_count != 0:
-            print("Diagnosis Count:", diagnosis_count)
-        else:
-            print(f"No diagnosis found from {start_time}")
 
