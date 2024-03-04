d269a7b8a54200f45a715b122fb7067fc631df50
Damodaran NJ <damodaran.swamy@accelnomics.com>
2024-03-04 23:00:46 +0000

    Changes on 20240304

---
+++ sim-central-ssc01/packs/sim_twilio/actions/send_sms.py
@@ -0,0 +1,20 @@
+from twilio.rest import Client
+
+from st2common.runners.base_action import Action
+
+
+class TwilioSendSMSAction(Action):
+    def __init__(self, config):
+        super(TwilioSendSMSAction, self).__init__(config=config)
+        self.client = Client(self.config['account_sid'], self.config['auth_token'])
+
+    def run(self, from_number, to_number, body):
+        try:
+            self.client.messages.create(body=body, from_=from_number, to=to_number, )
+        except Exception as e:
+            error_msg = ('Failed sending sms to: %s, exception: %s\n' %
+                         (to_number, str(e)))
+            self.logger.error(error_msg)
+            raise Exception(error_msg)
+
+        self.logger.info('Successfully sent sms to: %s\n' % to_number)
